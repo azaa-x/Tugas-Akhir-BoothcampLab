054abdc3d46269199dd07a69689ee128f786ad5c
azaa-x <n.andrean119@gmail.com>
2025-09-25 14:29:09 +0700

    Add nodes: Bank, UpenCV, Rosserial

---
+++ build/bank_auth/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ytta/nasywa_ws/src/bank_auth
+# Build directory: /home/ytta/nasywa_ws/build/bank_auth
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
